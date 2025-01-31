c1599e70a42bf1247cb8e9f74520fff27d144a03
Muhammad Qasim <malikqasim20051@gmail.com>
2025-01-31 15:41:29 +0500

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,10 @@ var swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
